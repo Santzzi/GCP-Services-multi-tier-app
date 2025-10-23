51cbf7b3ce8e5a0ed356d36a821166ef85236b76
spedrazzi <santiago.pedrazzi@externos-cl.cencosud.com>
2025-10-23 12:53:22 -0300

    coorect identation

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.0"
-  
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
   }
-  
+
   backend "gcs" {
     bucket = "gcp-track1-terraform-state"
     prefix = "dev/terraform.tfstate"
@@ -34,7 +34,7 @@ resource "google_project_service" "required_apis" {
     "servicenetworking.googleapis.com",
     "cloudresourcemanager.googleapis.com"
   ])
-  
+
   project            = var.project_id
   service            = each.value
   disable_on_destroy = false
@@ -43,9 +43,9 @@ resource "google_project_service" "required_apis" {
 # IAM Module
 module "iam" {
   source = "../../modules/iam"
-  
+
   project_id  = var.project_id
   environment = var.environment
-  
+
   depends_on = [google_project_service.required_apis]
 }
